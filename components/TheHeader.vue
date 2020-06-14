a359239df57e00992fa384024d70cafea669f4fa
cryptodoctor <emmanuelneni@gmail.com>
2020-06-14 12:20:56 +0300

    adding link to blog page and fixing responsivity of header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,13 +1,18 @@
 <template>
-  <b-navbar toggleable type="dark" variant="dark">
+  <b-navbar toggleable="sm" type="dark" variant="dark">
     <b-container>
       <b-navbar-brand to="/"
         ><b-img src="https://edicasoft.com/static/public/favicon.ico"></b-img>
         Test Task</b-navbar-brand
       >
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item to="/users">Users</b-nav-item>
-      </b-navbar-nav>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/blogs">Blogs</b-nav-item>
+          <b-nav-item to="/users">Users</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
     </b-container>
   </b-navbar>
 </template>
