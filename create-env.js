7e736172424aa50bdc5505381cea08e30fb248e9
cryptodoctor <emmanuelneni@gmail.com>
2020-06-17 23:48:25 +0300

    adding env create updates

--- create-env.js
+++ create-env.js
@@ -4,6 +4,8 @@ fs.writeFileSync(
   `
   BACKEND_USERS_ENDPOINT=${process.env.BACKEND_USERS_ENDPOINT}\n
   BACKEND_POSTS_ENDPOINT=${process.env.BACKEND_POSTS_ENDPOINT}\n
-  BASE_URL=${process.env.BASE_URL}
+  BASE_URL=${process.env.BASE_URL}\n
+  BACKEND_TODOS_ENDPOINT=${process.env.BACKEND_TODOS_ENDPOINT}\n
+  BACKEND_ENDPOINT=${process.env.BACKEND_ENDPOINT}
 `
 )
